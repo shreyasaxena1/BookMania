ae6dc30eb522910c6df64bf71d0788056aaf1927
shrey <shreyasaxena117@gmail.com>
2021-04-05 10:15:08 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/bookmania/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.bookmania
+
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.circularreveal.coordinatorlayout.CircularRevealCoordinatorLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        frameLayout=findViewById(R.id.frame)
+        navigationView=findViewById(R.id.navigationView)
+        setUpToolbar()
+        //toolbar - anywhere , actionbar - top of screen
+
+                                                         //which act toggle placed
+         val actionBarDrawerToggle = ActionBarDrawerToggle(this@MainActivity ,
+             drawerLayout,  // navigation drawer
+             R.string.open_drawer, // action string : act 11
+             R.string.close_drawer
+          )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle) // click listener on hamburger
+        actionBarDrawerToggle.syncState() // change hamburger icon to back icon and vice versa
+    }
+
+      fun setUpToolbar(){
+          setSupportActionBar(toolbar)
+          //null coz sometimes we wont have toolbar on screen
+        supportActionBar?.title = "Toolbar Title"
+          //for home button -> later change it to hamburger
+          supportActionBar?.setHomeButtonEnabled(true)
+          supportActionBar?.setDisplayHomeAsUpEnabled(true) // only btn define not working
+
+
+    }
+     //click listener on action bar to make it working
+                                       //menuitem -  home button
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id==android.R.id.home){ //hamburger id
+            drawerLayout.openDrawer(GravityCompat.START) // drawer open from left=
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
